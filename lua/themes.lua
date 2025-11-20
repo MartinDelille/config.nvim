d408024034ddb775fb8e9c43f7b1f54b3734577f
Martin Delille <martin@delille.org>
2025-11-20 13:12:41 +0100

    [themes] Add more themes

--- lua/themes.lua
+++ lua/themes.lua
@@ -1,6 +1,14 @@
 return {
 	{ "catppuccin/vim" },
 	{ "folke/tokyonight.nvim" },
+	{ "navarasu/onedark.nvim" },
+	{ "projekt0n/github-nvim-theme" },
+	{ "EdenEast/nightfox.nvim" },
+	{ "rebelot/kanagawa.nvim" },
+	{ "sainnhe/everforest" },
+	{ "sainnhe/gruvbox-material" },
+	{ "sainnhe/sonokai" },
+	{ "marko-cerovac/material.nvim" },
 	{
 		"sonph/onehalf",
 		config = function(plugin) vim.opt.rtp:append(plugin.dir .. "/vim") end,
