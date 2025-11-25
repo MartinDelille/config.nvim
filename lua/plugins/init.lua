b018caab0bfeac1f6935a79a1ebf43898df2b070
Martin Delille <martin@delille.org>
2025-11-25 16:33:38 +0100

    wip

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -7,4 +7,32 @@ return {
 	},
 	"rhysd/conflict-marker.vim",
 	{ "habamax/vim-godot", event = "VimEnter" },
+	{
+		"sontungexpt/url-open",
+		event = "VeryLazy",
+		cmd = "URLOpenUnderCursor",
+		config = function()
+			local status_ok, url_open = pcall(require, "url-open")
+			if not status_ok then return end
+			url_open.setup({})
+		end,
+	},
+	{
+		"magicmonty/sonicpi.nvim",
+		dependencies = {
+			"hrsh7th/nvim-cmp",
+			"kyazdani42/nvim-web-devicons",
+		},
+		opts = {
+			server_dir = "/Applications/Sonic Pi.app/Contents/Resources/app/server",
+			-- daemon_path = "/Applications/Sonic Pi.app/Contents/Resources/app/server/ruby/bin/daemon.rb",
+		},
+		ft = { "sonicpi" }, -- or "ruby" if Sonic Pi files use .rb
+		keys = {
+			{ "<leader>pp", "<cmd>SonicPiStart<cr>", desc = "Start Sonic Pi" },
+			{ "<leader>pt", "<cmd>SonicPiStop<cr>", desc = "Quit Sonic Pi" },
+			{ "<leader>ps", "<cmd>SonicPiSendBuffer<cr>", desc = "Run Buffer in Sonic Pi" },
+			{ "<leader>pl", "<cmd>SonicPiRunLine<cr>", desc = "Run Line in Sonic Pi" },
+		},
+	},
 }
