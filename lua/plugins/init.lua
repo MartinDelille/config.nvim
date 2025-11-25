608a336524458adfca60ee5428340eff71b9cbba
Martin Delille <martin@delille.org>
2025-11-25 16:32:04 +0100

    [colorizer] Fix setup

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,7 +1,10 @@
 return {
 	"nvim-lua/plenary.nvim",
 	"christoomey/vim-tmux-navigator",
-	"catgoose/nvim-colorizer.lua",
+	{
+		"catgoose/nvim-colorizer.lua",
+		config = function() require("colorizer").setup() end,
+	},
 	"rhysd/conflict-marker.vim",
 	{ "habamax/vim-godot", event = "VimEnter" },
 }
