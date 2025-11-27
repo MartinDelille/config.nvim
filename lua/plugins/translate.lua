8fa077dcb2702a63f11eb14ed74b8efa200c63f1
Martin Delille <martin@delille.org>
2025-11-27 18:45:24 +0100

    Add translate.nvim

---
+++ lua/plugins/translate.lua
@@ -0,0 +1,17 @@
+return {
+	"uga-rosa/translate.nvim",
+	config = function()
+		require("translate").setup({
+			default = {
+				command = "google",
+				source_lang = "auto",
+				target_lang = "en",
+				output = "replace",
+			},
+		})
+	end,
+	keys = {
+		{ "<leader>tr", "viw<cmd>Translate en -output=replace<CR>", mode = "n", desc = "Translate word under cursor and replace" },
+		{ "<leader>tr", "<cmd>Translate en -output=replace<CR>", mode = "v", desc = "Translate selection and replace" },
+	},
+}
