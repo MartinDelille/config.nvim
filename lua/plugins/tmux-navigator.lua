30e75ed2a0d719f490b6f5374b8a3e4157827223
Martin Delille <martin@delille.org>
2025-12-30 14:13:17 +0100

    [tmux-navigator] Improve configuration for terminal support

---
+++ lua/plugins/tmux-navigator.lua
@@ -0,0 +1,22 @@
+return {
+	"christoomey/vim-tmux-navigator",
+	cmd = {
+		"TmuxNavigateLeft",
+		"TmuxNavigateDown",
+		"TmuxNavigateUp",
+		"TmuxNavigateRight",
+		"TmuxNavigatePrevious",
+		"TmuxNavigatorProcessList",
+	},
+	keys = {
+		{ mode = "n", "<c-h>", "<cmd>TmuxNavigateLeft<cr>" },
+		{ mode = "n", "<c-j>", "<cmd>TmuxNavigateDown<cr>" },
+		{ mode = "n", "<c-k>", "<cmd>TmuxNavigateUp<cr>" },
+		{ mode = "n", "<c-l>", "<cmd>TmuxNavigateRight<cr>" },
+		{ mode = "t", "<c-h>", "<C-w><cmd>TmuxNavigateLeft<cr>" },
+		{ mode = "t", "<c-j>", "<C-w><cmd>TmuxNavigateDown<cr>" },
+		{ mode = "t", "<c-k>", "<C-w><cmd>TmuxNavigateUp<cr>" },
+		{ mode = "t", "<c-l>", "<C-w><cmd>TmuxNavigateRight<cr>" },
+	},
+	init = function() vim.g.tmux_navigator_no_mappings = 1 end,
+}
