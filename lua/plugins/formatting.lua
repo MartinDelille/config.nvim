18d77b7792f79e758b1e7de761addfcb4912c95c
Martin Delille <martin@delille.org>
2025-12-30 14:17:18 +0100

    [formatting] Fix black args and increase timeout

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -22,10 +22,15 @@ return {
 				gdshader = { "clang-format" },
 				sh = { "shfmt" },
 			},
+			formatters = {
+				black = {
+					prepend_args = { "--fast" },
+				},
+			},
 			format_on_save = function(bufnr)
 				-- Disable with a global or buffer-local variable
 				if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then return end
-				return { timeout_ms = 500, lsp_fallback = true }
+				return { timeout_ms = 5000, lsp_fallback = true }
 			end,
 		})
 
