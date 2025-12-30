31fde5a0e2126c03e4be63582f1b67106f6d915c
Martin Delille <martin@delille.org>
2025-12-30 16:44:59 +0100

    [keymaps] Remove window and tab management keymaps and use lua functions

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -9,36 +9,18 @@ local keymap = vim.keymap -- for conciseness
 -- use jk to exit insert mode
 keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 
--- clear search highlights
-keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
-
--- delete single character without copying into register
--- keymap.set("n", "x", '"_x')
-
 -- increment/decrement numbers
 keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
 keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
 
--- window management
-keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
-keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
-keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
-keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
-
-keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
-keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
-keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
-keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
-keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
-
 -- buffer management
-keymap.set("n", "<tab>", "<cmd>bn<CR>", { desc = "Go to next buffer" })
-keymap.set("n", "<S-tab>", "<cmd>bp<CR>", { desc = "Go to previous buffer" })
+keymap.set("n", "<tab>", function() vim.cmd.bnext() end, { desc = "Go to next buffer" })
+keymap.set("n", "<S-tab>", function() vim.cmd.bprevious() end, { desc = "Go to previous buffer" })
 
 -- Update lazy plugins
-keymap.set("n", "<leader>lz", "<cmd>Lazy update<CR>", { desc = "Update lazy plugins" })
+keymap.set("n", "<leader>lz", function() vim.cmd.Lazy("update") end, { desc = "Update lazy plugins" })
 
 -- Run make
-keymap.set("n", "<leader>mm", "<cmd>term make<CR>", { desc = "Run make" })
+keymap.set("n", "<leader>mm", function() vim.cmd.terminal("make") end, { desc = "Run make" })
 
-keymap.set("n", "<leader>yp", ':let @+ = expand("%:p")<CR>', { desc = "Yank file path to clipboard" })
+keymap.set("n", "<leader>yp", function() vim.fn.setreg("+", vim.fn.expand("%:p")) end, { desc = "Yank file path to clipboard" })
