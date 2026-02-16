b88d3e414e76dda41619748dd800aad6a1777132
Martin Delille <martin@delille.org>
2026-02-16 16:00:02 +0100

    [oil] Allow moving files with git

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -2,6 +2,11 @@ return {
 	"stevearc/oil.nvim",
 	---@module 'oil'
 	---@type oil.SetupOpts
+	opts = {
+		git = {
+			mv = function(src_path, dest_path) return true end,
+		},
+	},
 	keys = {
 		{ "-", function() vim.cmd("Oil") end, desc = "Open parent directory" },
 	},
