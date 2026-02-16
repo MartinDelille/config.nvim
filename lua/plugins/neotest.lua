cca293ee01c58933cb2057c3162dbd7e227abaf2
Martin Delille <martin@delille.org>
2026-02-16 16:01:06 +0100

    [neotest] Add keymaps for running tests in current file and opening output/summary panels

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -4,7 +4,27 @@ return {
 		{
 			"<leader>ts",
 			function() require("neotest").run.run() end,
-			"Run tests",
+			desc = "Run nearest tests",
+		},
+		{
+			"<leader>tw",
+			function() require("neotest").run.run(vim.fn.expand("%")) end,
+			desc = "Run all tests in current file",
+		},
+		{
+			"<leader>tw",
+			function() require("neotest").watch.watch(vim.fn.expand("%")) end,
+			desc = "Watch current test file",
+		},
+		{
+			"<leader>to",
+			function() require("neotest").output_panel.toggle() end,
+			desc = "Open neotest output panel (right)",
+		},
+		{
+			"<leader>tsu",
+			function() require("neotest").summary.toggle() end,
+			desc = "Open neotest summary panel (right)",
 		},
 	},
 	dependencies = {
