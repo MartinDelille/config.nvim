d5bf5217b298ee54580cfcb2b73c6a26f9964087
Martin Delille <martin@delille.org>
2026-02-18 14:40:55 +0100

    [plugins] Simpler init.lua

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,15 +1,7 @@
 return {
-	"nvim-lua/plenary.nvim",
-	"lewis6991/gitsigns.nvim",
-	{
-		"catgoose/nvim-colorizer.lua",
-		config = function() require("colorizer").setup() end,
-	},
-	"rhysd/conflict-marker.vim",
+	{ "catgoose/nvim-colorizer.lua", config = true },
+	{ "rhysd/conflict-marker.vim" },
 	{ "habamax/vim-godot", event = "VimEnter" },
-	{
-		"Kicamon/markdown-table-mode.nvim",
-		config = function() require("markdown-table-mode").setup() end,
-	},
-	"subnut/nvim-ghost.nvim",
+	{ "Kicamon/markdown-table-mode.nvim", config = true },
+	{ "subnut/nvim-ghost.nvim" },
 }
