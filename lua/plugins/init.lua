502a5c9f4d9950d73088f97f6efce0659eb786a5
Martin Delille <martin@delille.org>
2026-03-30 15:04:56 +0200

    [plugins/init.lua] Add mustache

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,7 +1,8 @@
 return {
-	{ "catgoose/nvim-colorizer.lua", config = true },
+	{ "catgoose/nvim-colorizer.lua", opt = {} },
 	{ "rhysd/conflict-marker.vim" },
 	{ "habamax/vim-godot", event = "VimEnter" },
 	{ "Kicamon/markdown-table-mode.nvim", config = true },
 	{ "ipkiss42/xwiki.vim" },
+	{ "mustache/vim-mustache-handlebars", ft = { "mustache" } },
 }
