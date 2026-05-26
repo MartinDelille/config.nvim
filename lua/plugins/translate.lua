4f962639964dd33acaa04cc68bda746b4f31b202
Martin Delille <martin@delille.org>
2026-05-26 06:59:35 +0200

    [translate] Improve keymaps

--- lua/plugins/translate.lua
+++ lua/plugins/translate.lua
@@ -3,10 +3,11 @@ require("translate").setup({
 	default = {
 		command = "google",
 		source_lang = "auto",
-		target_lang = "en",
 		output = "replace",
 	},
 })
 
-vim.keymap.set("n", "<leader>tr", "viw<cmd>Translate en -output=replace<CR>", { desc = "Translate word under cursor and replace" })
-vim.keymap.set("v", "<leader>tr", "<cmd>Translate en -output=replace<CR>", { desc = "Translate selection and replace" })
+vim.keymap.set("n", "<leader>tre", "viw<cmd>Translate en -output=replace<CR>", { desc = "Translate word under cursor in english and replace" })
+vim.keymap.set("n", "<leader>trf", "viw<cmd>Translate fr -output=replace<CR>", { desc = "Translate word under cursor in frech and replace" })
+vim.keymap.set("v", "<leader>tre", "<cmd>Translate en -output=replace<CR>", { desc = "Translate selection in english and replace" })
+vim.keymap.set("v", "<leader>trf", "<cmd>Translate fr -output=replace<CR>", { desc = "Translate selection in french and replace" })
