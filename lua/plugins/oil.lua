3e6a33c1b6fa33bb100cb0b2dabbd05cddd6982f
Martin Delille <mdelille@vivlio.com>
2026-05-28 17:26:59 +0200

    [oil] Add oil-git

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,6 +1,10 @@
-vim.pack.add({ "https://github.com/stevearc/oil.nvim" })
+vim.pack.add({
+  { src = "https://github.com/stevearc/oil.nvim" },
+  { src = "https://github.com/malewicz1337/oil-git.nvim" },
+})
 
 require("mini.icons").setup({})
+require("oil-git").setup({})
 
 local oil = require("oil")
 oil.setup({
