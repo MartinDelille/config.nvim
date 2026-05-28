64b038da7fec9c4dac6924f51823e291a48cc1f8
Martin Delille <mdelille@vivlio.com>
2026-05-28 17:25:54 +0200

    [init] Setup markdown-table-mode

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -9,6 +9,8 @@ vim.pack.add({
   { src = "https://github.com/kylechui/nvim-surround", version = vim.version.range("4.0.0 - 5.0.0") },
 })
 
+require("markdown-table-mode").setup()
+
 require("plugins/treesitter")
 require("plugins/oil")
 require("plugins/snacks-config")
