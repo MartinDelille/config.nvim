0111abcdf0dcbc7f37c9bce70d66ebe717a56875
Martin Delille <martin@delille.org>
2026-07-24 19:45:36 +0200

    [treesitter] Simpler configuration

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,51 +2,30 @@ vim.pack.add({ {
   src = "https://github.com/nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   branch = "main",
-}, "https://github.com/windwp/nvim-ts-autotag" })
+} })
 
-require("nvim-treesitter").setup({ -- enable syntax highlighting
-  highlight = {
-    enable = true,
-  },
-  -- enable indentation
-  indent = { enable = true },
-  -- enable autotagging (w/ nvim-ts-autotag plugin)
-  autotag = {
-    enable = true,
-  },
-  -- ensure these language parsers are installed
-  ensure_installed = {
-    "json",
-    "javascript",
-    "yaml",
-    "html",
-    "css",
-    "markdown",
-    "markdown_inline",
-    "python",
-    "bash",
-    "lua",
-    "vim",
-    "gitignore",
-    "query",
-    "vimdoc",
-    "c",
-    "cpp",
-    "gdscript",
-    "godot_resource",
-    "gdshader",
-    "glsl",
-    "angular",
-    "typescript",
-    "tsx",
-  },
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = "<C-space>",
-      node_incremental = "<C-space>",
-      scope_incremental = false,
-      node_decremental = "<bs>",
-    },
-  },
-})
+require("nvim-treesitter").install(
+  "json",
+  "javascript",
+  "yaml",
+  "html",
+  "css",
+  "markdown",
+  "markdown_inline",
+  "python",
+  "bash",
+  "lua",
+  "vim",
+  "gitignore",
+  "query",
+  "vimdoc",
+  "c",
+  "cpp",
+  "gdscript",
+  "godot_resource",
+  "gdshader",
+  "glsl",
+  "angular",
+  "typescript",
+  "tsx"
+)
